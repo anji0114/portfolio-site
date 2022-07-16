324e280257dadb160e16d2006dc60907a754ab79
anji0114 <anji.tnk.140114@gmail.com>
2022-07-16 14:04:56 +0900

    トップページhtml枠組み完了

--- front-page.php
+++ front-page.php
@@ -8,14 +8,95 @@
 <body>
   <?php get_template_part("includes/header"); ?>
   <div class="l-wrapper">
+    <div class="l-home-top">
+      <div class="p-home-visual">
+        <div class="l-container">
+          <div class="p-home-visual__content">
+            <h2 class="p-home-visual__title"></h2>
+            <p class="p-home-visual__text"></p>
+          </div>
+        </div><!-- /.l-container -->
+      </div><!-- /.p-home-visual -->
 
-    <div class="p-home-visual">
+      <section class="p-index-about">
+        <div class="l-container">
+          <div class="p-index-about__content">
+            <div class="c-heading01">
+              <h2 class="c-heading01__en"></h2>
+            </div>
+            <h3 class="p-index-about__subtitle"></h3>
+            <div class="p-index-about__texts">
+              <p class="p-index-about__text"></p>
+              <p class="p-index-about__text"></p>
+              <p class="p-index-about__text"></p>
+            </div>
+            <div class="c-buttons c-buttons--left p-index-about__buttons">
+              <a href="" class="c-buttons"></a>
+            </div>
+          </div><!-- /.p-index-about__content -->
+        </div><!-- /.l-container -->
+      </section><!-- /.p-index-about -->
 
+      <div class="l-home-top__image">
+        <img src="" alt="">
+      </div><!-- /.l-home-top__image -->
     </div>
 
     <main class="l-main">
-      <section class="p-index-about"></section>
-      <section class="p-index-works"></section>
+      <section class="p-index-works">
+        <div class="l-container">
+          <div class="c-heading01">
+            <p class="c-heading01__en"></p>
+            <h2 class="c-heading01__ja"></h2>
+          </div>
+          <div class="p-works__grid p-index-works__gird">
+            <div class="p-works__menu">
+              <ul class="p-works__categories c-categories">
+                <li class="p-works__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-works__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-works__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-works__category c-category">
+                  <a href=""></a>
+                </li>
+              </ul>
+            </div>
+            <div class="p-works__content">
+              <ul class="p-works__list">
+                <li class="p-works__item">
+                  <div class="p-works__image">
+
+                  </div>
+                  <div class="p-works__item-content">
+                    <ul class="p-works__item-categories">
+                      <li class="p-works__item-category">
+                        <a href=""></a>
+                      </li>
+                      <li class="p-works__item-category">
+                        <a href=""></a>
+                      </li>
+                    </ul>
+                    <p class="p-works__item-date"></p>
+                    <p class="p-works__item-title"></p>
+                    <span class="p-works__arrow"></span>
+                  </div>
+                  <a href=""></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="c-buttons p-index-works__buttons">
+            <a href="" class="c-button"></a>
+          </div>
+        </div>
+      </section>
+
+      <!-- p-index-blogs -->
       <section class="p-index-blogs">
         <div class="l-container">
           <div class="p-index-blogs__grid">

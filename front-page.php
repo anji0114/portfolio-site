069195433aa03df555ea27054ae1a063e47e529f
anji0114 <anji.tnk.140114@gmail.com>
2022-07-16 13:42:38 +0900

    コーディングファイル作成（Commitし忘れ）

---
+++ front-page.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php get_header(); ?>
+</head>
+
+<body>
+  <?php get_template_part("includes/header"); ?>
+  <div class="l-wrapper">
+
+    <div class="p-home-visual">
+
+    </div>
+
+    <main class="l-main">
+      <section class="p-index-about"></section>
+      <section class="p-index-works"></section>
+      <section class="p-index-blogs">
+        <div class="l-container">
+          <div class="p-index-blogs__grid">
+            <div class="p-index-blogs__heading">
+              <div class="c-heading01">
+                <p class="c-heading01__en"></p>
+                <h2 class="c-heading01__ja"></h2>
+              </div>
+              <ul class="p-index-blogs__categories c-categories">
+                <li class="p-index-blogs__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-index-blogs__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-index-blogs__category c-category">
+                  <a href=""></a>
+                </li>
+                <li class="p-index-blogs__category c-category">
+                  <a href=""></a>
+                </li>
+              </ul>
+            </div>
+            <div class="p-index-blogs__content">
+              <ul class="p-index-blogs__list">
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+                <li class="p-index-blogs__item">
+                  <div class="p-index-blogs__info">
+                    <p class="p-index-blogs__date"></p>
+                    <a href="" class="p-index-blogs__item-category"></a>
+                  </div>
+                  <h3 class="p-index-blogs__title">
+                    <a href=""></a>
+                  </h3>
+                </li>
+              </ul><!-- /.p-index-blogs__list -->
+              <div class="p-index-blogs__buttons c-buttons">
+                <a href="" class="c-button"></a>
+              </div>
+            </div><!-- /.p-index-blogs__content -->
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <section class="p-index-contact"></section>
+  </div>
+
+
+  <?php get_footer(); ?>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<header class="l-header p-header">
+  <div class="p-header__inner">
+    <div class="p-header__logo"></div>
+    <div class="p-header__burger"></div>
+    <nav class="p-header__nav">
+      <ul class="p-header__nav-list">
+        <li class="p-header__nav-item">
+          <a href=""></a>
+        </li>
+        <li class="p-header__nav-item">
+          <a href=""></a>
+        </li>
+        <li class="p-header__nav-item">
+          <a href=""></a>
+        </li>
+        <li class="p-header__nav-item">
+          <a href=""></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>

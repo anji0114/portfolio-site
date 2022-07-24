23a17d4a88ba751315614a55ea348ba9d06152ab
anji0114 <anji.tnk.140114@gmail.com>
2022-07-24 10:15:28 +0900

    コーディング

--- front-page.php
+++ front-page.php
@@ -12,8 +12,15 @@
       <div class="p-home-visual">
         <div class="l-container">
           <div class="p-home-visual__content">
-            <h2 class="p-home-visual__title"></h2>
-            <p class="p-home-visual__text"></p>
+            <h2 class="p-home-visual__title">
+              Welcome to<br />
+              PortFolio Site
+            </h2>
+            <p class="p-home-visual__text">
+              UI/UXデザイン、運用・改善、人材支援で、<br />
+              お客様のビジネスを成功へと導く<br />
+              デジタルクリエイティブの専門集団です。
+            </p>
           </div>
         </div><!-- /.l-container -->
       </div><!-- /.p-home-visual -->
@@ -22,7 +29,7 @@
         <div class="l-container">
           <div class="p-index-about__content">
             <div class="c-heading01">
-              <h2 class="c-heading01__en"></h2>
+              <h2 class="c-heading01__en">ABOUT</h2>
             </div>
             <h3 class="p-index-about__subtitle"></h3>
             <div class="p-index-about__texts">
@@ -38,7 +45,7 @@
       </section><!-- /.p-index-about -->
 
       <div class="l-home-top__image">
-        <img src="" alt="">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/mv.svg" alt="">
       </div><!-- /.l-home-top__image -->
     </div>
 
@@ -46,8 +53,8 @@
       <section class="p-index-works">
         <div class="l-container">
           <div class="c-heading01">
-            <p class="c-heading01__en"></p>
-            <h2 class="c-heading01__ja"></h2>
+            <p class="c-heading01__en">WORKS</p>
+            <h2 class="c-heading01__ja">制作実績</h2>
           </div>
           <div class="p-works__grid p-index-works__gird">
             <div class="p-works__menu">
@@ -68,25 +75,39 @@
             </div>
             <div class="p-works__content">
               <ul class="p-works__list">
-                <li class="p-works__item">
-                  <div class="p-works__image">
+                <?php
+                $args = array(
+                  'post_type' => 'works',
+                  'posts_per_page' => 5,
+                  'orderby' => 'date',
+                  'order' => 'DESC'
+                );
+                $my_blogs = new WP_Query($args);
+                if ($my_blogs->have_posts()) :
+                  while ($my_blogs->have_posts()) :
+                    $my_blogs->the_post();
+                ?>
+                    <li class="p-works__item">
+                      <div class="p-works__image">
+                      </div>
+                      <div class="p-works__item-content">
+                        <ul class="p-works__item-categories">
 
-                  </div>
-                  <div class="p-works__item-content">
-                    <ul class="p-works__item-categories">
-                      <li class="p-works__item-category">
-                        <a href=""></a>
-                      </li>
-                      <li class="p-works__item-category">
-                        <a href=""></a>
-                      </li>
-                    </ul>
-                    <p class="p-works__item-date"></p>
-                    <p class="p-works__item-title"></p>
-                    <span class="p-works__arrow"></span>
-                  </div>
-                  <a href=""></a>
-                </li>
+                          <li class="p-works__item-category">
+                            <a href=""></a>
+                          </li>
+                          <li class="p-works__item-category">
+                            <a href=""></a>
+                          </li>
+                        </ul>
+                        <p class="p-works__item-date"></p>
+                        <p class="p-works__item-title"><?php the_title(); ?></p>
+                        <span class="p-works__arrow"></span>
+                      </div>
+                      <a href=""></a>
+                    </li>
+                <?php endwhile;
+                endif; ?>
               </ul>
             </div>
           </div>
@@ -102,8 +123,8 @@
           <div class="p-index-blogs__grid">
             <div class="p-index-blogs__heading">
               <div class="c-heading01">
-                <p class="c-heading01__en"></p>
-                <h2 class="c-heading01__ja"></h2>
+                <p class="c-heading01__en">BLOG</p>
+                <h2 class="c-heading01__ja">ブログ</h2>
               </div>
               <ul class="p-index-blogs__categories c-categories">
                 <li class="p-index-blogs__category c-category">
@@ -122,60 +143,33 @@
             </div>
             <div class="p-index-blogs__content">
               <ul class="p-index-blogs__list">
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
-                <li class="p-index-blogs__item">
-                  <div class="p-index-blogs__info">
-                    <p class="p-index-blogs__date"></p>
-                    <a href="" class="p-index-blogs__item-category"></a>
-                  </div>
-                  <h3 class="p-index-blogs__title">
-                    <a href=""></a>
-                  </h3>
-                </li>
+                <?php
+                $args = array(
+                  'post_type' => 'post',
+                  'posts_per_page' => 5,
+                  'orderby' => 'date',
+                  'order' => 'DESC'
+                );
+                $my_blogs = new WP_Query($args);
+                if ($my_blogs->have_posts()) :
+                  while ($my_blogs->have_posts()) :
+                    $my_blogs->the_post();
+                ?>
+                    <li class="p-index-blogs__item">
+                      <div class="p-index-blogs__info">
+                        <p class="p-index-blogs__date">
+                          <?php the_time("Y.m.d"); ?>
+                        </p>
+                        <a href="" class="p-index-blogs__item-category">
+
+                        </a>
+                      </div>
+                      <h3 class="p-index-blogs__title">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                      </h3>
+                    </li>
+                <?php endwhile;
+                endif; ?>
               </ul><!-- /.p-index-blogs__list -->
               <div class="p-index-blogs__buttons c-buttons">
                 <a href="" class="c-button"></a>
--- templates/index-contact.php
+++ templates/index-contact.php
@@ -1,8 +1,10 @@
 <section class="p-index-contact">
   <div class="p-index-contact__inner">
-    <p class="p-index-contact__text"></p>
+    <p class="p-index-contact__text">
+      
+    </p>
     <div class="c-buttons p-index-contact__buttons">
-      <a href="" class="p-index-contact__button"></a>
+      <a href="" class="p-index-contact__button">Contact Us</a>
     </div>
   </div>
 </section>
